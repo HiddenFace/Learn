d9a25a015cce4e06268c8ed789ea0be48d9b66c3
HiddenFace <westinfo@bk.ru>
2017-04-24 00:52:17 +0300

    гавно

--- ep11.rb
+++
@@ -1,9 +0,0 @@
-def fasik_1(family_name="", first_name="")
-	greeting = "fasik kun\n"
-	unless family_name == "" && first_name == ""
-	greeting += "Vlad\n" + first_name + " " + family_name
-	end
-greeting
-end
-
-puts        fasik_1("ya to4no tak 4emunibud nau4us?", "ne, nu 9 prosto ne yveren")
