d4b7d08121f9458804d34918d63d45ea4024af6d
v.artemyev <vlad@meyvndigital.co.uk>
2017-04-29 13:44:20 +0300

    My fixes

--- person.rb
+++ person.rb
@@ -6,12 +6,45 @@ class Person
 
   attr_accessor :first_name, :last_name, :patronymic, :birthdate, :location
 
+
+  class << self
+
+    attr_accessor :persons
+
+    def generate(amount:)
+      self.persons = []
+      amount.times do
+        person = self.new(first_name: Utils.generate_first_name,
+                          last_name:  Utils.generate_last_name,
+                          patronymic: Utils.generate_patronymic,
+                          location:   Utils.generate_location,
+                          birthdate:  Utils.generate_birthdate)
+
+        self.persons.push(person)
+      end
+    end
+
+    def as_json
+      self.persons.map(&:to_hash).to_json
+    end
+
+    def store
+      File.open('persons.json', 'w+') do |file|
+        file.puts(self.as_json)
+      end
+    end
+
+    def read
+      JSON.load(File.read('persons.json'))
+    end
+  end
+
   def initialize(first_name: , last_name:, patronymic:, birthdate:, location:)
     @first_name = first_name
-    @last_name = last_name
+    @last_name  = last_name
     @patronymic = patronymic
-    @birthdate = birthdate
-    @location = location
+    @birthdate  = birthdate
+    @location   = location
   end
 
   def full_name
@@ -31,27 +64,30 @@ class Person
   end
 
   def to_hash
-    {first_name: @first_name, last_name: @last_name, patronymic: @patronymic, birthdate: @birthdate.iso8601, location: @location}
+    { first_name: @first_name, last_name: @last_name, patronymic: @patronymic, birthdate: @birthdate.iso8601, location: @location }
   end
 
 end
 
-persons = []
+Person.generate(amount: 100)
+Person.store
 
-100.times do
-  person = Person.new(first_name: Utils.generate_first_name,
-                      last_name:  Utils.generate_last_name,
-                      patronymic: Utils.generate_patronymic,
-                      location:   Utils.generate_location,
-                      birthdate:  Utils.generate_birthdate).to_hash
+p Person.persons
+p Person.as_json
+p Person.read
 
-  persons.push(person)
-end
+# persons = []
 
-persons.each do |person|
-  puts person
-end
+# 100.times do
+#   person = Person.new().to_hash
 
-File.open("persons_JSON.json", "w+") do |file|
-  file.puts(persons.to_json)
-end
+#   persons.push(person)
+# end
+
+# persons.each do |person|
+#   puts person
+# end
+
+# File.open("persons_JSON.json", "w+") do |file|
+#   file.puts(persons.to_json)
+# end
