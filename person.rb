9b5b73745102add2f07c07109c0dce61c96c1273
HiddenFace <westinfo@bk.ru>
2017-06-15 02:23:43 +0300

    extented method select

--- person.rb
+++ person.rb
@@ -36,13 +36,20 @@ class Person
     self.persons
   end
 
-  def select(first_name:, last_name:)
+  # def select(first_name:, last_name:)
+  #   self.persons || self.all
+  #   self.persons.select do |person|
+  #     person.first_name == first_name && person.last_name == last_name
+  #   end
+  # end
+  def select(args)
     self.persons || self.all
     self.persons.select do |person|
-      person.first_name == first_name && person.last_name == last_name
+      args.all? do |k, v|
+      person.send(k) == v
+      end
     end
   end
-
 
     def as_json
       self.persons.map(&:to_hash).to_json
@@ -98,6 +105,35 @@ end
 #   puts person.details
 # end
 
-puts Person.select(first_name: 'Милан', last_name: 'Веселов').map(&:details)
-puts Person.select(first_name: 'Хуй', last_name: 'Моржовый').map(&:details)
-puts Person.select(first_name: 'Милан', last_name: 'Орлов').map(&:details)
+# puts Person.select(first_name: 'Милан', last_name: 'Моржовый').map(&:details)
+# puts Person.select(first_name: 'Хуй', last_name: 'Моржовый').map(&:details)
+# puts Person.select(first_name: 'Милан', last_name: 'Моржовый').map(&:details)
+
+
+# метод all? Enumerable.
+# метод send
+# хэш который будет лежать в переменной аrgs
+# def select(args:)
+#   "args хэш, хэш обходим eachом и внутри метода select испольхуем метод all? на все аргументы и внутри этого all?(это тоже итератор) сравниваем каждый из
+#     них с персон и если all? вернет тру, то тогда селект будет считать, что это валидная запись."
+#     args один аргумент, но этим аргументом будет большой хэш ластнаме = хуемое и.т.п т.е. и в этом хеше все вычисления.
+#  send - т.е.при обходе своего хэша берем ключ этого хэша и прокидывать в метод send таким образом будет вызываться метод и можно получить
+#     значение непосредственного персона и просравнять его со значением хеша, который я передал аргументом и соответсвенно если они равны то в контексте метода
+#      алл все заработает и если все значения равны все заселектится.
+
+#     Enumerable all? [1,2,3,4].all? do i == число? end
+#       ключ хеша который передам аргументом и буду сравнивать с аттрибутом персона
+#       ессли в методе all? буду брать ключ хеша который передам аргументом и буду сравнивать с аттрибутом персона, который находится в итерации.
+
+
+
+puts Person.select(first_name: 'Лабута', last_name: 'Егоров', age: 36).map(&:details)
+#   Person.send(:all.first)
+#   a = Person.send(:all).first
+#   #<Person:0x00000006942b30 @first_name="Мадлен", @last_name="Иванов", @patronymic="Адамович", @birthdate=1974-09-27 00:00:00 +0300, @location="Всеволожск">
+#   a
+# => #<Person:0x00000006942b30 @first_name="Мадлен", @last_name="Иванов", @patronymic="Адамович", @birthdate=1974-09-27 00:00:00 +0300, @location="Всеволожск">
+# a.send(:first_name)   => "Мадлен"
+# b = [:first_name, :last_name, :age]   => [:first_name, :last_name, :age]
+# b.map{ |x| a.send(x)}   => ["Мадлен", "Иванов", 42]
+# х = это метод(фирстнаме, ластнаме, эйдж)
